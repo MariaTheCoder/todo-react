2944f489b54aa2421e326c5df6eada8269909418
Maria T. Andersen <mteilmann93@gmail.com>
2022-06-23 21:14:12 +0200

    remove code which is commented out

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,9 +1,6 @@
-// import { useState } from "react";
 import "./ToDo.css";
 
 export default function ToDo(props) {
-  //   const [todo, setTodo] = useState("");
-
   return (
     <li className="ToDo">
       <label>
