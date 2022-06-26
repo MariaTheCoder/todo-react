06908c444073bf17a69aab9beaaabf79d372d155
Maria T. Andersen <mteilmann93@gmail.com>
2022-06-26 07:44:45 +0200

    Add line-through on list elements (to-dos) when checkbox gets checked

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import "./ToDo.css";
 
 export default function ToDo(props) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = (event) => {
+    if (event.target.checked) {
+      console.log("I was checked");
+      event.target.className = "Selected";
+    } else {
+      console.log("I was un-checked");
+    }
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <li className="ToDo">
+    <li className={isChecked === true ? "ToDo Selected" : "ToDo"}>
       <label>
-        <input type="checkbox" />
+        <input type="checkbox" onChange={handleChange} />
         {props.todo}
       </label>
     </li>
