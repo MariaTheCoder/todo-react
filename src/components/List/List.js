4966bba18be2406b759b50717488bef01be72e9b
Maria T. Andersen <mteilmann93@gmail.com>
2022-06-26 06:46:35 +0200

    Add style to hr element

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,6 +5,7 @@ export default function List(props) {
   return (
     <div className="List">
       <h1>To-do List</h1>
+      <hr className="hr"></hr>
       <div>
         {props.todos.map((todo, index) => (
           <ToDo key={index} todo={todo} />
