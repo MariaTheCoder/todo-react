a094179fbe06a67d85127ce8d0351a7b14d357ed
Maria T. Andersen <mteilmann93@gmail.com>
2022-07-04 20:18:41 +0200

    Upon click on edit button, change todo into an input element of type text. Make sure that the value matches the todo text from before the edit button was clicked

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -2,6 +2,24 @@ import "./ToDo.css";
 import CreateEditButton from "../EditButton/CreateEditButton";
 
 export default function ToDo(props) {
+  const isEditable = props.todo.inEditMode;
+
+  if (isEditable) {
+    return (
+      <li className="ToDo">
+        <label>
+          <input
+            type="checkbox"
+            onChange={() => props.checkTodo(props.todo.id)}
+            id={props.todo.id}
+            checked={props.todo.isChecked}
+          />
+          <input type="text" id={props.todo.id} value={props.todo.task} />
+        </label>
+        <CreateEditButton todo={props.todo} editTodo={props.editTodo} />
+      </li>
+    );
+  }
   return (
     <li className="ToDo">
       <label>
