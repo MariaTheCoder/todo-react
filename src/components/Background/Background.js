fc59225f7140079198704a3833ae3648fa8dcd2c
Maria T. Andersen <mteilmann93@gmail.com>
2022-07-20 22:06:54 +0200

    Restructure code

---
+++ src/components/Background/Background.js
@@ -0,0 +1,10 @@
+import "./Background.css";
+import Note from "../Note/Note";
+
+export default function Background() {
+  return (
+    <div class="Background">
+      <Note />
+    </div>
+  );
+}
