659a1476ea63f954f4e62f152cef5e71cd6ab05d
Maria T. Andersen <mteilmann93@gmail.com>
2022-07-21 19:39:38 +0200

    Restructure code to fix bug regarding the top margin and clean up code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,69 @@
+import { useState } from "react";
 import "./App.css";
-import Background from "../Background/Background";
+import CreateComponent from "../CreateComponent/CreateComponent";
+import List from "../List/List";
+import Actionbar from "../Actionbar/Actionbar";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoToEdit, setTodoToEdit] = useState("");
+  const [inputHasChanged, setInputHasChanged] = useState(false);
+
+  function addTodo(newTodo) {
+    if (newTodo.task.length === 0) return alert("Please write a text");
+    setTodos([...todos, newTodo]);
+  }
+
+  function checkTodo(id) {
+    const newListOfTodos = [...todos];
+    const foundTodo = newListOfTodos.find((todo) => todo.id === id);
+    foundTodo.isChecked = !foundTodo.isChecked;
+
+    setTodos(newListOfTodos);
+  }
+
+  function toggleInEditMode(id) {
+    const newListOfTodos = [...todos];
+    const foundTodo = newListOfTodos.find((todo) => todo.id === id);
+    foundTodo.inEditMode = !foundTodo.inEditMode;
+
+    setTodos(newListOfTodos);
+  }
+
+  function saveTodoEdit(id) {
+    const newListOfTodos = [...todos];
+    const foundTodo = newListOfTodos.find((todo) => todo.id === id);
+
+    console.log(inputHasChanged);
+
+    if (!inputHasChanged) return;
+    foundTodo.task = todoToEdit;
+    setTodos(newListOfTodos);
+    setInputHasChanged(false);
+  }
+
+  function deleteSpecificTodo(id) {
+    const newListOfTodos = [...todos];
+    const indexOfFoundTodo = newListOfTodos.findIndex((todo) => todo.id === id);
+    newListOfTodos.splice(indexOfFoundTodo, 1);
+
+    setTodos(newListOfTodos);
+  }
+
   return (
     <div className="App">
-      <Background />
+      <CreateComponent todos={todos} addTodo={addTodo} />
+      <List
+        todos={todos}
+        checkTodo={checkTodo}
+        toggleInEditMode={toggleInEditMode}
+        todoToEdit={todoToEdit}
+        setTodoToEdit={setTodoToEdit}
+        saveTodoEdit={saveTodoEdit}
+        deleteSpecificTodo={deleteSpecificTodo}
+        setInputHasChanged={setInputHasChanged}
+      />
+      <Actionbar todos={todos} setTodos={setTodos} />
     </div>
   );
 }
